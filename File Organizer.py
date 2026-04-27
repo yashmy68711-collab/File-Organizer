007517e881828e781b9a968099a63d47282f03fc
Yash <yashmy68711@gmail.com>
2026-04-27 10:36:36 +0530

    Update File Organizer.py

--- File Organizer.py
+++ File Organizer.py
@@ -11,36 +11,41 @@ if not os.path.exists(path):
 
 files = os.listdir(path)
 
+file_types = {
+    "Images": (".jpg", ".png", ".jpeg"),
+    "Documents": (".pdf", ".docx", ".txt"),
+    "Videos": (".mp4", ".mkv"),
+    "Archives": (".zip", ".rar")
+}
+
 for file in files:
     file_path = os.path.join(path, file)
-    if os.path.isdir(file_path):
-        continue
+     if os.path.isdir(file_path) or file.startswith("."):
+        continue  
     try:
-        if file.endswith((".jpg", ".png", ".jpeg")):
-            folder = "Images"
+        moved = False
+        for folder, extensions in file_types.items():
+            if file.lower().endswith(extensions):
+                folder_path = os.path.join(path, folder)
 
-        elif file.endswith((".pdf", ".docx", ".txt")):
-            folder = "Documents"
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
 
-        elif file.endswith((".mp4", ".mkv")):
-            folder = "Videos"
+                shutil.move(file_path, os.path.join(folder_path, file))
+                print(f"Moved: {file} → {folder}")
+                moved = True
+                break
 
-        elif file.endswith((".zip", ".rar")):
-            folder = "Archives"
+        if not moved:
+            other_folder = os.path.join(path, "Others")
+      
+            if not os.path.exists(other_folder):
+                os.makedirs(other_folder)
 
-        else:
-            folder = "Others"
-
-        folder_path = os.path.join(path, folder)
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        # Move file
-        shutil.move(file_path, os.path.join(folder_path, file))
-
-        print(f"Moved: {file} → {folder}")
+            shutil.move(file_path, os.path.join(other_folder, file))
+            print(f"Moved: {file} → others")
 
     except Exception as e:
         print(f"Error moving {file}: {e}")
 
-print("Files organized successfully!")
+print("Files organized successfully!")):
